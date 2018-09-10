8a3f3a6f21fc1122c40e028c964efa8f1f57b551
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-09-10 14:26:30 +0530

    [IMP] license in manifest for branch 10.0

--- server_action/__manifest__.py
+++ server_action/__manifest__.py
@@ -17,7 +17,8 @@ Functionalities
     'website': "http://www.aktivsoftware.com/",
 
     'category': 'Server',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
