b8714de9642a1df628965aee3a46ee3ed6ab3bcd
odooaktiv <odoo@aktivsoftware.com>
2020-07-03 18:21:40 +0530

    [IMP] set category

--- server_action/__manifest__.py
+++ server_action/__manifest__.py
@@ -16,7 +16,7 @@ Functionalities
     'author': "Aktiv software ",
     'website': "http://www.aktivsoftware.com/",
 
-    'category': 'Server',
+    'category': 'Extra Tools',
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
 
